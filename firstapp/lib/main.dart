43d6ffc1dc5bc0c29478a64ed8943dc163d535ba
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-06-29 20:40:40 +0100

    added images and button/icons

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -17,8 +17,9 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: const Center(
-        child: Text(
+      body: Center(
+        //text
+        /*child: Text(
           'Hello world',
           style: TextStyle(
             fontSize: 20.0,
@@ -27,7 +28,39 @@ class Home extends StatelessWidget {
             color: Colors.amber,
             fontFamily: 'IndieFlower',
           ),
+        ),*/
+        
+        //images
+        /*child: Image(
+          //network img
+          //image: NetworkImage('https://images.unsplash.com/photo-1688019984360-50d40dfa955a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+
+          //img from pc
+          image: AssetImage('assets/IMG_0599.jpg'),
+        ),*/
+
+        //buttons/icons
+        /*child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50.0,
+        ),*/
+        //button with icon
+        /*child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              Icons.mail,
+            ),
+            label: Text('mail me'),
+        ),*/
+        //icon button
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.abc),
+          color: Colors.red,
         ),
+
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
