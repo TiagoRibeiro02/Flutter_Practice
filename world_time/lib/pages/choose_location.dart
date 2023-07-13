91ff69944d076d8a028abdbb72c39e7026c1d72c
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-07-13 16:01:35 +0100

    packages

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -9,25 +9,7 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    //simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
 
-    //simulate network request to get bio of user
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'vegan, musician';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
 
 
   @override
