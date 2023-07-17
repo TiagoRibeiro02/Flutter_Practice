5b0bcc4bd175181fd3717def951a8291a997676a
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-07-17 19:39:31 +0100

    view done

--- tic_tac_toe/lib/home_page.dart
+++ tic_tac_toe/lib/home_page.dart
@@ -8,8 +8,173 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int scoreO = 0;
+  int scorex = 0;
+  bool turnOfX = true;
+  int BoxesFull = 0;
+  List<String> xOrOList = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () { refresh(); },
+              icon: Icon(Icons.refresh),
+          )
+        ],
+
+        title: Center(
+          child: Text(
+            'Tic Tac Toe',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+
+        backgroundColor: Colors.grey[800],
+      ),
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          buidscore(),
+          buildGrid(),
+          buildTurnOf(),
+        ],
+      ),
+    );
+  }
+
+  Widget buidscore() {
+    return Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Player X',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                          ),
+                      ),
+                      const SizedBox(height: 20.0,),
+                      Text(
+                        scorex.toString(),
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Player O',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      const SizedBox(height: 20.0,),
+                      Text(
+                        scoreO.toString(),
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+        )
+    );
+  }
+
+  Widget buildGrid() {
+    return Expanded(
+      flex: 3,
+        child: GridView.builder(
+          itemCount: 9,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3
+            ),
+            itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    clicked();
+                  },
+                  child: Container(
+                    decoration:
+                    BoxDecoration(
+                      border: Border.all(color: Colors.grey)
+                    ),
+                    child: Center(
+                      child: Text(
+                        xOrOList[index],
+                        style: TextStyle(
+                          color: xOrOList[index] == 'x' ? Colors.white : Colors.red,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+            }
+        )
+    );
+  }
+
+  Widget buildTurnOf() {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          turnOfX ? 'Turn of X' : 'Turn of O',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void clicked() {
+
+  }
+
+
+
+  void refresh() {
+
   }
 }
